e58552adb8098d9a8476ec444a5fe4dae3d964eb
Kevin Roa <kevinroa71@gmail.com>
2020-11-20 18:11:37 -0400

    docs(plantilla de clasificacion usando naive bayes): creacion de la plantilla y finalizacion del modulo clasificacion con naive bayes del curso

---
+++ ejercicios/naive-bayes/plantilla.py
@@ -0,0 +1,49 @@
+# Librerias
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+# Importo el dataset
+dataset = pd.read_csv('./ejercicios/naive-bayes/Social_Network_Ads.csv')
+x = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1].values
+
+# Separar conjunto de entrenamiento y testing
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Escalar los datos
+from sklearn.preprocessing import StandardScaler
+scaler  = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test  = scaler.transform(x_test)
+
+# Construir mi modelo de naive bayes
+from sklearn.naive_bayes import GaussianNB
+clasificador = GaussianNB()
+clasificador.fit(x_train, y_train)
+
+# Predecir el conjunto de testing
+y_predic = clasificador.predict(x_test)
+
+# Elaborar una matriz de confusion
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_true=y_test, y_pred=y_predic)
+
+# Representación gráfica de los resultados del algoritmo en el Conjunto de testing
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, clasificador.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Clasificador (Conjunto de testing)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
